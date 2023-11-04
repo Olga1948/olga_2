a63f7c3afc96b0d08622e912c6ea396a80c6de03
Dmitry <Dr.Sleep1995@gmail.com>
2023-11-04 18:01:37 +0300

    Создайте ViewSet, который позволяет получить список всех пользователей из базы данных. Пользователи должны иметь возможность получить список всех пользователей в формате JSON, используя метод GET.

--- Serializer_Django_Project/Serializer_App/serializers.py
+++ Serializer_Django_Project/Serializer_App/serializers.py
@@ -1,5 +1,12 @@
 from rest_framework import serializers
 from .models import Product, Rating, Comment
+from django.contrib.auth.models import User
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'first_name', 'last_name')
 
 
 class ProductSerializer(serializers.ModelSerializer):
