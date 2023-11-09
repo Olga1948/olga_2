965d1548c8869a24767f01100b884637e3f6c653
Dmitry <Dr.Sleep1995@gmail.com>
2023-11-09 20:24:03 +0300

    связала бот с БД

--- olga_telebot/main.py
+++ olga_telebot/main.py
@@ -1,13 +1,15 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.types import ReplyKeyboardMarkup, ReplyKeyboardRemove, KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from dotenv import load_dotenv
 
 import logging
 import os
+import psycopg2
 
 
 load_dotenv()
 API_TOKEN = os.getenv('API_TOKEN')
+DATABASE_URL = 'postgresql://username:password@localhost:5432/olga_db'
 
 bot = Bot(token=API_TOKEN)
 
@@ -15,6 +17,8 @@ logging.basicConfig(level=logging.INFO)
 
 dp = Dispatcher(bot)
 
+conn = psycopg2.connect(DATABASE_URL, sslmode='prefer')
+cursor = conn.cursor()
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
@@ -23,6 +27,15 @@ async def start_command(message: types.Message):
     keyboard.add(button)
     await message.reply(text='Привет, я твой бот! Чем могу помочь?', reply_markup=keyboard)
 
+
+@dp.message_handler(commands=['викторина'])
+async def get_viktorina_command(message: types.Message):
+    cursor.execute('SELECT question, answer FROM qa')
+    qa_data = cursor.fetchall()
+
+    for row in qa_data:
+        question, answer = row
+        await message.reply(text=f'Вопрос: {question}\nОтвет: {answer}')
 
 if __name__ == '__main__':
     from aiogram import executor
